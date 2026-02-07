6118affa01d2c9fb3e0f909f3a2df4a8354b59a2
Phillip Alday <me@phillipalday.com>
2026-02-06 19:12:54 -0600

    update links for new repo ownership (#47)
    * update links for new repo ownership
    
    * Bump version from 2.3.0 to 2.3.1
    
    * update docs repo
    
    * Remove arch from CI, add Codecov slug

--- docs/make.jl
+++ docs/make.jl
@@ -2,11 +2,11 @@ using Documenter
 using MixedModelsExtras
 
 makedocs(;
-         repo=Remotes.GitHub("palday", "MixedModelsExtras.jl"),
+         repo=Remotes.GitHub("JuliaMixedModels", "MixedModelsExtras.jl"),
          sitename="MixedModelsExtras",
          doctest=true,
          checkdocs=:exports,
          warnonly=[:cross_references],
          pages=["index.md", "api.md"])
 
-deploydocs(; repo="github.com/palday/MixedModelsExtras.jl.git", push_preview=true)
+deploydocs(; repo="github.com/JuliaMixedModels/MixedModelsExtras.jl.git", push_preview=true)
